ccc702c08af86e1bca590edf45ad46a0650e9e60
James Landers <>
2021-11-28 15:58:22 -0500

    Added basic terraform implementation files.

---
+++ terraform.tfvars
@@ -0,0 +1,1 @@
+root_parent_id = "9682e83f-0be2-4e4e-8a62-4525fa142985"
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "root_parent_id" {
+  type        = string
+  description = "The root_parent_id is used to specify where to set the root for all Landing Zone deployments. Usually the Tenant ID when deploying the core Enterprise-scale Landing Zones."
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9-_\\(\\)\\.]{1,36}$", var.root_parent_id))
+    error_message = "Value must be a valid Management Group ID, consisting of alphanumeric characters, hyphens, underscores, periods and parentheses."
+  }
+}
