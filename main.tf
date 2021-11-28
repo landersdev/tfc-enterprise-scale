4dd745e31a6862ba96b6970e0afaa1c437c523cd
James Landers <>
2021-11-28 16:01:18 -0500

    Added call to main caf module.

---
+++ main.tf
@@ -0,0 +1,12 @@
+module "caf-enterprise-scale" {
+  source  = "Azure/caf-enterprise-scale/azurerm"
+  version = "1.1.0"
+
+  providers = {
+    azurerm              = azurerm
+    azurerm.connectivity = azurerm
+    azurerm.management   = azurerm
+  }
+
+  
+}
